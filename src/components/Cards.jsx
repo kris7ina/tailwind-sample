e55f0616738256a1ef7130a55e5305cdc6e05d28
kristina rulina <kristinarulina@gmail.com>
2023-08-19 21:53:02 +0300

    minor fixes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,7 +6,7 @@ import Triple from '../assets/triple.png'
 const Cards = () => {
   return (
     <div className='w-full py-[10rem] px-4 bg-white'>
-      <div className='max-w-[1040px] mx-auto grid md:grid-cols-3 gap-8 px-4'>
+      <div className='max-w-[1040px] mx-auto grid lg:grid-cols-3 gap-8 px-4'>
 
         <div className='w-full mx-auto max-w-[400px] shadow-xl shadow-cyan-800/10 flex flex-col p-4 my-8 rounded-lg hover:scale-105 duration-300'>
           <img className='w-20 mx-auto mt-[-3rem]' src={Single} alt='person' />
